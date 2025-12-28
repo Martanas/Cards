cc9dcf442dde974f5d82def1e7255537647c5ad3
Martynas Prišmantas <martynas.prismantas@gmail.com>
2025-12-28 14:46:10 +0200

    Implement LastCardsAvgRankingBot

---
+++ game/highlow/src/commonMain/kotlin/com/martanhub/game/highlow/bot/LastCardsAvgRankingBot.kt
@@ -0,0 +1,31 @@
+package com.martanhub.game.highlow.bot
+
+import com.martanhub.card.PlayingCard
+import com.martanhub.card.Rank
+import com.martanhub.game.highlow.CircularQueue
+import com.martanhub.game.highlow.Player
+
+class LastCardsAvgRankingBot(
+    private val rankFactory: Rank.Factory,
+    count: Int
+) : Player {
+    init {
+        require(count > 0) { "Bot must memorize at least one card" }
+    }
+
+    private val cardsQueue = CircularQueue<PlayingCard>(capacity = count)
+
+    override val name = "Bot: Last cards ranking"
+
+    override suspend fun guess(card: PlayingCard): Boolean {
+        cardsQueue.add(card)
+        val avg = cardsQueue.getAll()
+            .sumOf { it.rank.value }
+            .takeIf { it > 0 }
+            ?.div(cardsQueue.getAll().size)
+            ?: card.rank.value
+        val highestValue = rankFactory.highest().value
+        val lowestValue = rankFactory.lowest().value
+        return highestValue - avg > avg - lowestValue
+    }
+}
---
+++ game/highlow/src/commonTest/kotlin/com/martanhub/game/highlow/bot/LastCardsAvgRankingBotTest.kt
@@ -0,0 +1,63 @@
+package com.martanhub.game.highlow.bot
+
+import com.martanhub.card.FrenchPlayingCard
+import com.martanhub.card.FrenchRank
+import com.martanhub.card.FrenchSuit
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class LastCardsAvgRankingBotTest {
+    private val bot = LastCardsAvgRankingBot(FrenchRank.Factory, 3)
+
+    @Test
+    fun `given two, guesses higher`() = runTest {
+        assertTrue(bot.guess(FrenchPlayingCard(FrenchRank.TWO, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given seven, guesses higher`() = runTest {
+        assertTrue(bot.guess(FrenchPlayingCard(FrenchRank.SEVEN, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given eight, guesses lower`() = runTest {
+        assertFalse(bot.guess(FrenchPlayingCard(FrenchRank.EIGHT, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given ace, guesses lower`() = runTest {
+        assertFalse(bot.guess(FrenchPlayingCard(FrenchRank.ACE, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given ace, king and seven guesses lower`() = runTest {
+        bot.guess(FrenchPlayingCard(FrenchRank.ACE, FrenchSuit.DIAMONDS))
+        bot.guess(FrenchPlayingCard(FrenchRank.KING, FrenchSuit.DIAMONDS))
+
+        assertFalse(bot.guess(FrenchPlayingCard(FrenchRank.SEVEN, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given two, six and eight guesses higher`() = runTest {
+        bot.guess(FrenchPlayingCard(FrenchRank.TWO, FrenchSuit.DIAMONDS))
+        bot.guess(FrenchPlayingCard(FrenchRank.SIX, FrenchSuit.DIAMONDS))
+
+        assertTrue(bot.guess(FrenchPlayingCard(FrenchRank.EIGHT, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given ace and six guesses lower`() = runTest {
+        bot.guess(FrenchPlayingCard(FrenchRank.ACE, FrenchSuit.DIAMONDS))
+
+        assertFalse(bot.guess(FrenchPlayingCard(FrenchRank.SIX, FrenchSuit.DIAMONDS)))
+    }
+
+    @Test
+    fun `given two and king guesses higher`() = runTest {
+        bot.guess(FrenchPlayingCard(FrenchRank.TWO, FrenchSuit.DIAMONDS))
+
+        assertTrue(bot.guess(FrenchPlayingCard(FrenchRank.KING, FrenchSuit.DIAMONDS)))
+    }
+}
